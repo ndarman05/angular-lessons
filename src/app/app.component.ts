3ff0835bf7a537f28a267aae7a68012334510e94
sbeka <sbeka1996@gmail.com>
2019-06-14 14:52:20 +0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  item = '—— User ——';
+
+  getItem(event) {
+    this.item = event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-item/user-item.component';
+import { UserItemComponent } from './user-list/user-item/user-item.component';
+import {ContactsService} from '../shared/services/contacts.service';
+import { FormItemComponent } from './user-list/form-item/form-item.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserListComponent,
-    UserItemComponent
+    UserItemComponent,
+    FormItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule
   ],
-  providers: [],
+  providers: [
+      ContactsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
