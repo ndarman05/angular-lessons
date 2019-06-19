69fa07d65cbe0d0ce6dc1eb60788bab2ae7c0fd9
sbeka <sbeka1996@gmail.com>
2019-06-19 15:15:13 +0600

    restart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-list/user-item/user-item.component';
-import {ContactsService} from '../shared/services/contacts.service';
-import { FormItemComponent } from './user-list/form-item/form-item.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserListComponent,
-    UserItemComponent,
-    FormItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
       FormsModule
   ],
   providers: [
-      ContactsService
   ],
   bootstrap: [AppComponent]
 })
