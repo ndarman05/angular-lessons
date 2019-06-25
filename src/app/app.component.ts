7d82d7c1bed21fb1020aaa5b2047a8415ee201f9
Darmanbek Nauryzbayev <darman_96@inbox.ru>
2019-06-26 02:59:53 +0600

    Form Register first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  errors = [
+    {
+      name: 'required',
+      text: 'This field is required',
+      rules: ['touched', 'dirty'],
+    },
+    { name: 'minlength', text: 'Min length is 5', rules: ['dirty'] },
+    { name: 'maxlength', text: 'Max length is 16', rules: ['dirty'] }
+  ];
+  profileDate = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]),
+  });
+
+
+onSubmit(){
+  if(this.profileDate.status == "VALID"){
+    console.log(this.profileDate)
+  } else {
+    
+  }
+}
+
+isControlInvalid(controlName: string): boolean {
+  const control = this.profileDate.controls[controlName];
+   const result = control.invalid && control.touched;
+   return result;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { from } from 'rxjs';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
